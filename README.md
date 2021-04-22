3da4b045a753acfa36df2f698217ba6a7fe82ba0
Paul Symons <paul.symons@mechanicalrock.io>
2021-04-22 11:34:07 +0800

    committing results

--- README.md
+++ README.md
@@ -1,14 +1,46 @@
-# Welcome to your CDK TypeScript project!
+# Why does GlueContext modify the URL of a Glue Connection (JDBC)?
 
-This is a blank project for TypeScript development with CDK.
+## Context
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+When a Glue Connection is created - specifically a JDBC type - users can specify
 
-## Useful commands
+- URL
+- Username
+- Password
 
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+In my example, the url is as follows:
+
+> jdbc:oracle:thin://@hostname.rds.amazonaws.com:1521/ORCL
+
+This is fairly typical for a `connection_type = oracle` Glue Connection.
+
+## Problem
+
+When authoring spark jobs that intend to **write to a JDBC destination**, it seems to make sense to attempt the use of the Glue Connection to set up a writer.
+
+The [documentation for the GlueContext class](https://docs.aws.amazon.com/glue/latest/dg/aws-glue-api-crawler-pyspark-extensions-glue-context.html#aws-glue-api-crawler-pyspark-extensions-glue-context-extract_jdbc_conf) has two methods which indicate doing so should be possible:
+
+- write_from_jdbc_conf
+- extract_jdbc_conf
+
+The problem experienced in both of these methods, is that the Glue Connection URL is modified by these methods:
+
+> jdbc:oracle:thin://@hostname.rds.amazonaws.com:1521/ORCL
+
+loses the `thin` part to become
+
+> jdbc:oracle://@hostname.rds.amazonaws.com:1521/ORCL
+
+It's not clear why this happens and it causes problems when trying to write a dynamic frame in Glue Jobs.
+
+## Reproduce
+
+Use the cdk project within this repository to deploy
+
+- A standalone VPC
+- The sample Glue Connection
+- A Development Endpoint
+
+Once the project is running, go to the AWS Glue Console and create a new Sagemaker Notebook, connected to the Development Endpoint.
+
+From there, you can run the `ipynb` file in the `documents/` folder. Or you can simply view the screenshot in there, also.
--- lib/glue-bad-jdbc-stack.ts
+++ lib/glue-bad-jdbc-stack.ts
@@ -1,9 +1,100 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as iam from "@aws-cdk/aws-iam";
+import {
+  Vpc,
+  SubnetType,
+  GatewayVpcEndpointAwsService,
+  InterfaceVpcEndpointAwsService,
+  SecurityGroup,
+  Port,
+} from "@aws-cdk/aws-ec2";
+import * as glue from "@aws-cdk/aws-glue";
+import { ConnectionType } from "@aws-cdk/aws-glue";
 
 export class GlueBadJdbcStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const vpc = new Vpc(this, "bad-jdbc-vpc", {
+      cidr: "192.168.4.0/23",
+      maxAzs: 1,
+      subnetConfiguration: [
+        {
+          name: "glue",
+          subnetType: SubnetType.ISOLATED,
+          cidrMask: 26,
+        },
+        {
+          name: "public",
+          subnetType: SubnetType.PUBLIC,
+          cidrMask: 26,
+        },
+      ],
+      natGateways: 0,
+    });
+
+    // add s3 gateway endpoint
+    vpc.addGatewayEndpoint("s3-endpoint", {
+      service: GatewayVpcEndpointAwsService.S3,
+      subnets: [
+        {
+          subnetType: SubnetType.ISOLATED,
+        },
+      ],
+    });
+
+    // add glue interface endpoint
+    vpc.addInterfaceEndpoint("glue endpoint", {
+      service: InterfaceVpcEndpointAwsService.GLUE,
+      subnets: {
+        subnetType: SubnetType.ISOLATED,
+      },
+    });
+
+    // create a self referential security group because glue
+    const glueSecurityGroup = new SecurityGroup(this, "glue-sg", {
+      description: "glue self referential allow in out",
+      vpc,
+    });
+    glueSecurityGroup.connections.allowFrom(glueSecurityGroup, Port.allTcp());
+    glueSecurityGroup.connections.allowTo(glueSecurityGroup, Port.allTcp());
+
     // The code that defines your stack goes here
+    const glueConnect = new glue.Connection(this, "glue-connection", {
+      type: ConnectionType.JDBC,
+      connectionName: "bad-jdbc",
+      description: "part of a support case, please do not delete",
+      subnet: vpc.isolatedSubnets[0],
+      securityGroups: [glueSecurityGroup],
+      properties: {
+        JDBC_CONNECTION_URL:
+          "jdbc:oracle:thin://@hostname.rds.amazonaws.com:1521/ORCL",
+        USERNAME: "",
+        PASSWORD: "",
+      },
+    });
+
+    const glueRole = new iam.Role(this, "glue-role", {
+      assumedBy: new iam.ServicePrincipal("glue.amazonaws.com"),
+    });
+    const glueManagedPolicy = iam.ManagedPolicy.fromManagedPolicyArn(
+      this,
+      "glue-managed-policy",
+      "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+    );
+    glueRole.addManagedPolicy(glueManagedPolicy);
+
+    const devEndpoint = new glue.CfnDevEndpoint(this, "dev-endpoint", {
+      roleArn: glueRole.roleArn,
+      endpointName: "bad-jdbc-dev-endpoint",
+      workerType: "Standard",
+      glueVersion: "1.0",
+      arguments: {
+        GLUE_PYTHON_VERSION: "3",
+      },
+      numberOfWorkers: 2,
+      securityGroupIds: [glueSecurityGroup.securityGroupId],
+      subnetId: vpc.isolatedSubnets[0].subnetId,
+    });
   }
 }
